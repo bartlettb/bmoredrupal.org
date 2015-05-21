75a76920844c8d05f6483de57db1e31fb1bfbedd
sajeevp <sajeevpoovathingal@gmail.com>
2015-05-21 22:13:54 +0530

    New theme

---
+++ sites/all/themes/bmoredrupal/js/custom.js
@@ -0,0 +1,41 @@
+
+(function($) {'use strict';
+new WOW().init();
+ $(document).ready(function(){
+	 $('#header').height($(window).height()+'px');
+	 $('#block-block-1').height($(window).height()+'px'); 
+	 var vid = document.getElementById("videobg");
+	vid.playbackRate = 0.5;
+ });	 
+ $(window).load(function(){'use strict';
+		var sponsors_selectors = $('.sponsors-filter li a');
+		var sponsors = $('.sponsors-items');
+		sponsors.isotope({
+			itemSelector : '.sponsors-item',
+			layoutMode : 'fitRows'
+		});
+		
+		$(sponsors_selectors).click(function(){
+			$(sponsors_selectors).removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			sponsors.isotope({ filter: selector });
+			return false;
+		});
+	});
+	$(window).scroll(function(){
+    var $nav = $('#block-system-main-menu'); 
+    if ($('html').scrollTop() > 0) {
+            $nav.stop().animate({
+                opacity:'0'
+            });
+    }else{
+            $nav.stop().animate({
+                opacity:'1'
+            });
+        }  
+});
+})(jQuery);
+ 
+
+
